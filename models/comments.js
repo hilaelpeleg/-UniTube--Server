6a8180625a2da8a26f643945ffda8218b81f2fa7
hilaelpeleg <183hila@gmail.com>
2024-08-14 18:38:59 +0300

    update comments

--- models/comments.js
+++ models/comments.js
@@ -8,11 +8,6 @@ const commentSchema = new Schema({
         ref: 'Video',
         required: true,
     },
-    commentId: {
-        type: Number,
-        required: true,
-        unique: true,
-    },
     profilePicture: {
         type: String,
         default: 'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg',
