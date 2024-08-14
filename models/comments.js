81fdf84bf24392a82afc47155e3ce7bc165ce82f
hilaelpeleg <183hila@gmail.com>
2024-08-14 15:42:42 +0300

    update services and controllers comments

--- models/comments.js
+++ models/comments.js
@@ -15,7 +15,7 @@ const commentSchema = new Schema({
     },
     profilePicture: {
         type: String,
-        required: true,
+        default: 'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg',
     },
     name: {
         type: String,
