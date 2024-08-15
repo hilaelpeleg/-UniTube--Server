06c1be918e5ba4fe63c4fd2513381f6024cd4f55
hilaelpeleg <183hila@gmail.com>
2024-08-15 19:10:58 +0300

    update func names of comments

--- routes/comment.js
+++ routes/comment.js
@@ -1,16 +1,16 @@
 import express from 'express';
-import { getCommentsByVideoId, addComment, editComment, deleteComment } from '../controllers/comment.js';
+import { getComments, createComment, updateComment, deleteComment } from '../controllers/comment.js';
 
 const router = express.Router();
 
 // Get all comments for a specific video
-router.get('/:videoId', getCommentsByVideoId);
+router.get('/:videoId', getComments);
 
 // Add a new comment to a specific video
-router.post('/:videoId', addComment);
+router.post('/:videoId', createComment);
 
 // Edit a comment by its ID
-router.put('/:commentId', editComment);
+router.put('/:commentId', updateComment);
 
 // Delete a comment by its ID
 router.delete('/:commentId', deleteComment);
