8030aa241217b004546afaf1969033859965dce1
hilaelpeleg <183hila@gmail.com>
2024-09-17 17:15:05 +0300

    update routes

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,10 @@ import userController from '../controllers/user.js';
 
 const router = express.Router();
 
-router.get('/user', userController.getUser);
+// router.get('/user', userController.getUser);
 router.get('/users', userController.getAllUsers);
 router.post('/user',userController.createUser);
-router.delete('/users/:userName', userController.deleteUser);
+router.delete('/:userName', userController.deleteUser);
+router.get('/:userName',userController.getUser);
 
 export default router;
