5b6b18fb1fcedcc6eb9abcc03a0be27ee5d5fac7
OrelShai <orelshai991010@gmail.com>
2024-10-10 12:25:17 +0300

    add code comment to video routes

--- routes/video.js
+++ routes/video.js
@@ -3,18 +3,25 @@ import videoController from '../controllers/video.js';
 import { validateToken } from '../models/token.js';
 const router = express.Router();
 
+// Route to get all videos
 router.get('/', videoController.getVideos);
+
+// Route to get the video with the highest ID
 router.get('/highest-id', videoController.getHighestVideoId);
 
+// Route to increment video views by video ID
 router.post('/:pid/increment',videoController.incrementVideoViews);
+
+// Route to update the video duration by video ID
 router.put('/:pid', videoController.updateVideoDuration);
 
-
-// update likes
+// Route to update likes on a video, requires token validation
 router.put('/:pid/like', validateToken, videoController.updateVideoLikes);
 
-
+// Route to toggle like on a video by video ID
 router.post('/:videoId/like', videoController.toggleLike);
+
+// Route to toggle dislike on a video by video ID
 router.post('/:videoId/dislike', videoController.toggleDislike);
 
 export default router;
